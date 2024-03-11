15a86a63fb65862207062ca432f642c6d85b51b7
Tadeas1155 <85305306+Tadeas1155@users.noreply.github.com>
2024-03-11 18:41:39 +0100

    d

---
+++ githubreplacer.py
@@ -0,0 +1,5 @@
+link = "https://github.com/Tadeas1155/tokens/blob/main/martin.jpg"
+
+# note: this will break if a repo/organization or subfolder is named "blob" -- would be ideal to use a fancy regex
+# to be more precise here
+print(link.replace("github.com", "raw.githubusercontent.com").replace("/blob/", "/"))
